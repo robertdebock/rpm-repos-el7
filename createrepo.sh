fd26dedc8fc7450a48932d16de594467eba6f380
Robert de Bock <robert@meinit.nl>
2016-03-10 11:18:33 +0100

    Create createrepo.sh

---
+++ createrepo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+yum -y install createrepo
+mkdir /data/repository/
+cp /data/rpmbuild/RPMS/noarch/* /data/repository/
+cd /data/repository/
+createrepo .
+cat << EOF >> index.html
+<!DOCTYPE html>
+<html>
+<title>RPM index</title>
+<body>
+EOF
+ 
+ls *.rpm | while read rpm ; do
+ echo "<a href=/${rpm}>${rpm}</a><br/>" >> index.html
+done
+
+cat << EOF >> index.html
+ </body>
+</html> 
+EOF
